d3d949d98c6a6ea79d2b33e35cc2d0fead94ac78
yy_yank <yy.yank.me@gmail.com>
2026-05-02 21:10:23 +0900

    Use ESM Tailwind typography plugin import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
@@ -76,6 +78,6 @@ export default {
     }
   },
   plugins: [
-    require('@tailwindcss/typography')
+    typography
   ]
 };
